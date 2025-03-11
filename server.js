f3bb3ff7e1b5f365e9950fd297e53cc2527b5e3b
salimzayd <zidsalim786@gmail.com>
2025-03-11 12:09:45 +0530

    db

--- server.js
+++ server.js
@@ -33,11 +33,10 @@ app.use(cors(corsOptions));
 
 
 // Database Connection
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+})
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
